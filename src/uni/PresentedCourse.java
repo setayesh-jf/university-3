aa155cec238e25f916ad63625c76bb8f67cef614
setayesh-jf <setayeshsadafiii@gmail.com>
2025-03-05 07:16:48 +0330

    successfuly upload little change

--- src/uni/PresentedCourse.java
+++ src/uni/PresentedCourse.java
@@ -7,31 +7,32 @@ public class PresentedCourse {
     public int courseID;
     public int professorID;
     public int capacity;
-    public ArrayList<Integer> studentIds;
+    public ArrayList<Integer> studentIds = new ArrayList<>();
 
     public PresentedCourse (int courseID, int professorID, int maxCapacity){
+        this.id = presentedCourseList.size() + 1;
         this.courseID = courseID;
         this.professorID = professorID;
         this.capacity = maxCapacity;
-        this.studentIds = new ArrayList<>(capacity);
-        this.id = presentedCourseList.size();
+        presentedCourseList.add(this);
+
     }
 
-     public static PresentedCourse findById (int ID){
-         for (PresentedCourse presentedCourse : presentedCourseList){
-             if (presentedCourse.id == ID){
-                 return presentedCourse;
-             }
-         }
-         return null;
-     }
+    public static PresentedCourse findById (int ID){
+        for (PresentedCourse presentedCourse : presentedCourseList){
+            if (presentedCourse.id == ID){
+                return presentedCourse;
+            }
+        }
+        return null;
+    }
 
     public void addStudent (int studentID){
         if (studentIds.size() < capacity){
             studentIds.add(studentID);
         }
         else {
-            System.out.println("error");
+            System.out.println("error! Zarfiat por shodeh");
         }
     }
 }
